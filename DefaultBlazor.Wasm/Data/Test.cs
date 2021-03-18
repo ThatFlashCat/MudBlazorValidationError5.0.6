9858bcb313974c5a620fb4808eda2a4c2029e566
Riley Nielsen <rnielsen@srfconsulting.com>
2021-03-18 09:43:13 -0500

    Add project files.

---
+++ DefaultBlazor.Wasm/Data/Test.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DefaultBlazor.Wasm.Data
+{
+    public class Test
+    {
+        [Test]
+        public string SomeString1 { get; set; }
+        [Test]
+        public string SomeString2 { get; set; }
+    }
+}
---
+++ DefaultBlazor.Wasm/Data/TestAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DefaultBlazor.Wasm.Data
+{
+    public class TestAttribute : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var propertyTestedInfo = validationContext.ObjectType.GetProperty(""); //error will show up here. The validationContext is null after the control loses focus
+
+            return ValidationResult.Success;
+        }
+    }
+}
